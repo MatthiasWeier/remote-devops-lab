2440a9e170afedc0bd75f9aed07fbfee9ceb3244
Matthias Weier <matthias.weier@web.de>
2026-05-07 22:34:01 +0200

    Get rid of old stuff

--- terraform/provider.tf
+++
@@ -1,5 +0,0 @@
-data "oci_identity_availability_domains" "ads" {
-  # OLD: compartment_id = "ocid1.tenancy.oc1..aaaaaaa..."
-  # NEW: We point it to the variable bucket! No quotation marks!
-  compartment_id = var.tenancy_ocid
-}
